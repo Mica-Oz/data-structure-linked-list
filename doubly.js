f2c9124d500e7a6372d3b34ffbc7595194b9dd69
Mica Oz <kahulaoz@gmail.coml>
2022-06-18 18:51:08 -0400

    ok

--- doubly.js
+++ doubly.js
@@ -66,9 +66,91 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+  unshift(val) {
+    let newNode = new Node(val);
+    let oldHead = this.head;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      oldHead.prev = newNode;
+      this.head = newNode;
+      this.head.next = oldHead;
+    }
+    this.length++;
+    return this;
+  }
+  get(id) {
+    if (id >= this.length || id < 0) return null;
+    if (id <= this.length / 2) {
+      let currentNode = this.head;
+      let count = 0;
+      while (count <= id) {
+        if (count === id) {
+          return currentNode;
+        }
+        currentNode = currentNode.next;
+        count++;
+      }
+    } else {
+      let currentNode = this.tail;
+      let count = this.length - 1;
+      while (count >= id) {
+        if (count === id) {
+          return currentNode;
+        }
+        currentNode = currentNode.prev;
+        count--;
+      }
+    }
+  }
+  set(n, val) {
+    let node = this.get(n);
+    if (node) {
+      node.val = val;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  insert(id, val) {
+    if (id < 0 || id > this.length) return false;
+    if (id === this.length) return !!this.push(val); //bang bang gives us true as return
+    if (id === 0) !!this.unshift(val);
+
+    let leftNode = this.get(id - 1);
+    let rightNode = this.get(id);
+    let currNode = new Node(val);
+
+    leftNode.next = currNode;
+    currNode.next = rightNode;
+    rightNode.prev = currNode;
+    currNode.prev = leftNode;
+    this.length++;
+    return true;
+  }
+
+  remove(id) {
+    if (id < 0 || id > this.length) return undefined;
+    if (id === this.length - 1) return this.pop();
+    if (id === 0) return this.shift();
+    let left = this.get(id - 1);
+    let curr = this.get(id);
+    let right = curr.next;
+    left.next = right;
+    right.prev = left;
+    curr.prev = null;
+    curr.next = null;
+    this.length--;
+    return curr;
+  }
 }
 
 let list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
 list.push(3);
+list.push(4);
+list.push(5);
+list.push(6);
+list.push(7);
